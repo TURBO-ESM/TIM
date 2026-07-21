43deeee8d615120c13f1d74fd98deaabfb2fb3b7
Michael Waxmonsky <mwaxmonsky@ucar.edu>
2026-07-21 16:43:25 -0600

    Update boundary exchange organization.

--- test_mom/boundary/test_boundary_exchange.cpp
+++ test_mom/boundary/test_boundary_exchange.cpp
@@ -18,17 +18,26 @@ TEST(BoundaryExchange, BoundaryMatchesAfterOneTimeStep) {
     test_mom::CapturedFile captured(test_mom::data_dir /
                                     ("MOM_fixed_initialization_" + std::format("{:04}", myRank)));
 
-    auto bathyTBefore = captured.fab_host("G%bathyT_brefore");
-    auto bathyTAfter  = captured.fab_host("G%bathyT_after"  );
+    // Number of halo cells in x,y directions
     int  nihalo       = captured.integer("G%Domain%nihalo");
     int  njhalo       = captured.integer("G%Domain%njhalo");
+    // Total computational domain not including ghost cells
     int  niglobal     = captured.integer("G%Domain%niglobal");
     int  njglobal     = captured.integer("G%Domain%njglobal");
+    // Global location of current grid with ghost cells counting negative at origin
+    // using 0-based indexing
+    int  isd_global   = captured.integer("G%isd_global");
+    int  jsd_global   = captured.integer("G%jsd_global");
+
+    auto bathyTBefore = captured.fab_host("G%bathyT_brefore", isd_global, jsd_global);
+    auto bathyTAfter  = captured.fab_host("G%bathyT_after"  , isd_global, jsd_global);
+
+    // Sets up data domain region
+    int iLocalDataGridSize = bathyTBefore.box().length(0) - (2*nihalo);
+    int jLocalDataGridSize = bathyTBefore.box().length(1) - (2*njhalo);
 
     amrex::BoxList     boxList;
     amrex::Vector<int> processBoxList;
-    int iLocalDataGridSize = bathyTBefore.box().length(0) - (2*nihalo);
-    int jLocalDataGridSize = bathyTBefore.box().length(1) - (2*njhalo);
 
     // Assumes equally sized grids
     for(int i = 0; i < amrex::ParallelDescriptor::NProcs(); i++)
@@ -36,8 +45,9 @@ TEST(BoundaryExchange, BoundaryMatchesAfterOneTimeStep) {
         test_mom::CapturedFile rankc(test_mom::data_dir /
                                     ("MOM_fixed_initialization_" + std::format("{:04}", i)));
 
-        int iGridStartLoc = rankc.integer("G%bathyT_brefore%isd_global") + nihalo;
-        int jGridStartLoc = rankc.integer("G%bathyT_brefore%jsd_global") + njhalo;
+        // Each rank needs to map the region every other rank convers.
+        int iGridStartLoc = rankc.integer("G%isd_global") + nihalo;
+        int jGridStartLoc = rankc.integer("G%jsd_global") + njhalo;
         amrex::IntVect lo(iGridStartLoc, jGridStartLoc, 0);
         amrex::IntVect hi(lo[0] + iLocalDataGridSize - 1, lo[1] + jLocalDataGridSize - 1, 0);
 

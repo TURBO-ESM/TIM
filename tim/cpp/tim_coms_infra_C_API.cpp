51d7b30baa9f35f29454a0f08c303abf70485b00
mwaxmonsky <137746677+mwaxmonsky@users.noreply.github.com>
2026-09-23 09:06:02 -0600

    [FULL REVIEW] Field checksum update (#2)
    Updates C++ implementation and C-redirects to use AMReX structures instead of C/Fortran-based parameters.

--- tim/cpp/tim_coms_infra_C_API.cpp
+++ tim/cpp/tim_coms_infra_C_API.cpp
@@ -6,6 +6,29 @@
 #include "tim_coms_infra_C_API.h"
 #include "tim_coms_infra.hpp"
 
-int64_t tim_chksum_c(double* field, size_t field_size, double* mask_val) {
-    return TIM::checksum(field, field_size, mask_val);
+#include <AMReX_Arena.H>
+#include <AMReX_Gpu.H>
+
+int64_t tim_chksum_c(const RealArray_C* field_HOST, double* mask_val)
+{
+    amrex::Box bx({0, 0, 0},
+                  {field_HOST->shape[0], field_HOST->shape[1], field_HOST->shape[2]});
+
+    size_t num_points = bx.numPts();
+
+    amrex::Real* device_array = static_cast<amrex::Real*>(amrex::The_Arena()->alloc(num_points * sizeof(amrex::Real)));
+    amrex::Gpu::copy(amrex::Gpu::hostToDevice, field_HOST->data, field_HOST->data+num_points, device_array);
+
+    amrex::BaseFab<amrex::Real> non_owning_fab(bx, num_points, device_array);
+    auto array_1d = non_owning_fab.array();
+
+    ///-------------------------------------------------
+    /// Execute checksum
+    ///-------------------------------------------------
+    int64_t chksum = mask_val ? TIM::checksum(bx, array_1d, *mask_val)
+                              : TIM::checksum(bx, array_1d);
+
+    amrex::The_Arena()->free(device_array);
+
+    return chksum;
 }
